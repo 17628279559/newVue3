146bd579e19be74b9a3ed245826088aec34b81d6
snaketao <z17628279559@163.com>
2022-04-22 23:46:28 +0800

    asd

--- vue/src/views/force/force.vue
+++ vue/src/views/force/force.vue
@@ -1,31 +1,15 @@
 <script setup>
 import { onMounted, watch, getCurrentInstance } from "vue"
-import miserables from './data.json'
-import links from './data2.json'
+import miserables from './data1.json'
 import ForceGraph from './ForceGraph'
-import newForce from './newForce'
 const { proxy } = getCurrentInstance()
-
-const chart1 = newForce(links)
 
 const chart2 = ForceGraph(miserables, {
   nodeId: d => d.id,
   nodeGroup: d => d.group,
-  nodeTitle: d => `${d.id}\n${d.group}`,
-  linkStrokeWidth: l => Math.sqrt(l.value),
   width: 1000,
   height: 540,
   //   invalidation // a promise to stop the simulation when the cell is re-run
-})
-
-watch(proxy.$force, (newValue, oldValue) => {
-  if (newValue == 1) {
-    $("#force").children().remove()
-    $("#force").append(chart1)
-  } else {
-    $("#force").children().remove()
-    $("#force").append(chart2)
-  }
 })
 
 onMounted(() => {
